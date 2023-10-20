219884fbb6e1b139b1eb93c5d484f74fe8831592
h.samadi <samadi.hadis@gmail.com>
2023-10-20 12:28:10 +0330

    Review

--- app/src/main/java/com/samadihadis/weatherapplication/MainActivity.kt
+++ app/src/main/java/com/samadihadis/weatherapplication/MainActivity.kt
@@ -3,7 +3,7 @@ package com.samadihadis.weatherapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.samadihadis.weatherapplication.databinding.ActivityMainBinding
 import okhttp3.Call
 import okhttp3.Callback
@@ -41,19 +41,26 @@ class MainActivity : AppCompatActivity() {
 
                 val weatherArray = jsonObject.getJSONArray("weather")
                 val weatherObject = weatherArray.getJSONObject(0)
+                val iconId = weatherObject.getString("icon")
+                val imageUrl = "https://openweathermap.org/img/wn/${iconId}10d@2x.png"
 
 
                 runOnUiThread {
-                    showContent(jsonObject.getString("name"), weatherObject.getString("description"))
+                    showContent(
+                        jsonObject.getString("name"),
+                        weatherObject.getString("description"),
+                        imageUrl
+                    )
                 }
             }
         })
     }
 
 
-    fun showContent(cityName: String , weatherDescription : String) {
+    fun showContent(cityName: String, weatherDescription: String, imageUrl: String) {
        binding.cityName.text = cityName
         binding.weatherDescription.text = weatherDescription
+        Glide.with(this@MainActivity).load(imageUrl).into(binding.imageViewWeather)
     }
 
 
